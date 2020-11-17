4114478713920604f90a53e8c720525eb8ad9aac
Alexander Strakh <astrakh@PMINTL.NET>
2020-11-18 02:02:16 +0300

    Fix bugs

--- src/main/java/com/blockwit/kafka/security/examples/SSLConstants.java
+++ src/main/java/com/blockwit/kafka/security/examples/SSLConstants.java
@@ -4,10 +4,10 @@ public class SSLConstants {
 
     public static final String SSL_CLIENT_KEYSTORE_PASSWORD = "clientKeystorePassword";
 
-    public static final String SSL_CLIENT_TRUSTSTORE_PASSWORD = "clientTruststorePassword";
+    public static final String SSL_CLIENT_TRUSTSTORE_PASSWORD = "kafka12345";
 
     public static final String SSL_CLIENT_KEYSTORE_PATH = "/path/to/client/keystore/client.keystore.p12";
 
-    public static final String SSL_CLIENT_TRUSTSTORE_PATH = "/path/to/client/truststore/client.truststore.jks";
+    public static final String SSL_CLIENT_TRUSTSTORE_PATH = "/home/iceberg/expr/client.truststore.jks";
 
 }
